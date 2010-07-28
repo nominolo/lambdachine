cc25cf6fca44d073d2a9c3d99807c8e86ac33edb
Thomas Schilling <nominolo@googlemail.com>
2010-07-28 23:24:21 +0100

    Document Unique and add a few more helpers.
    Name spaces are now only 7 bits.

--- compiler/Lambdachine/Utils/Unique.hs
+++ compiler/Lambdachine/Utils/Unique.hs
@@ -3,36 +3,77 @@ module Lambdachine.Utils.Unique (
   {-uniqueFromInt,-} intFromUnique, newUniqueSupply,
   bogusUnique,
   Supply, split, split2, split3, split4, supplyValue, modifySupply,
-  mkBuiltinUnique
+  mkBuiltinUnique, fromExternalUnique
 ) where
 
 import Lambdachine.Utils.Pretty
 
 import Data.Array.Unboxed
-import Data.Bits ( shiftL, (.|.), (.&.) )
-import Data.Char ( ord )
+import Data.Bits ( shiftL, shiftR, (.|.), (.&.) )
+import Data.Char ( ord, chr )
 import Data.Supply
 
+-- | A 'Unique' is simply an integer value.  It is used to quickly
+-- compare two objects for identity.  A finite map indexed by objects
+-- with an associated 'Unique' may also be implemented more
+-- efficiently.
+--
+-- A unique consists of a 7 bit name space (usually written as a
+-- 'Char') and a 24 bit number.  The name space (see first argument to
+-- 'newUniqueSupply') allows us to have several independent unique
+-- supplies.  (E.g., one for primitives, one for external symbols,
+-- etc.)
+--
+-- 'Unique's are pretty-printed compactly by using a large base
+-- representation (around base 64).  E.g., A 'Unique' with namespace
+-- @a@ and number @54321@ is written as @axQN@
 newtype Unique = Unique Int
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
 
 instance Pretty Unique where
-  ppr (Unique u) 
-    | u /= 0 = text (toBaseXString (u .&. 0x00ffffff))
+  ppr u0@(Unique u) 
+    | u /= 0 = text (show u0)
     | otherwise = empty
 
+instance Show Unique where
+  show u0@(Unique u) =
+    uniqueNameSpace u0 : toBaseXString (u .&. 0x00ffffff)
+
+-- | Create a new 'Unique' supply.  The first argument encodes the
+-- name space (as a character for easier printing).
+--
+-- It is up to the user to not create multiple supplies with the same
+-- name space.
 newUniqueSupply :: Char -> IO (Supply Unique)
-newUniqueSupply c | ord c < 256 =
+newUniqueSupply c | ord c < 128 =
   newSupply u0 (\(Unique n) -> Unique (n + 1))
  where
    u0 = Unique ((ord c `shiftL` 24) .|. 1)
 newUniqueSupply _ = error "newUniqueSupply: char must be 8 bits"
 
+-- | Use only for defining 'Unique's for built-in types.  Uses name
+-- space @%@.
 mkBuiltinUnique :: Int -> Unique
-mkBuiltinUnique = mk_unique 'p'
+mkBuiltinUnique = mk_unique '%'
+
+-- | Turn a GHC Unique into our 'Unique'.
+--
+-- To ensure that no GHC Unique overlaps with locally generated
+-- Uniques, we tag GHC-imported uniques by setting the highest bit
+-- (the sign bit).  Assumes that GHC name spaces are at most 7 bits.
+--
+fromExternalUnique :: Int -> Unique
+fromExternalUnique n
+  | n .&. 0x80000000 == 0 = Unique (n .|. 0x80000000)
+  | otherwise = error $ "fromGhcUnique: GHC Unique too large."
+
+-- | Return the name space of the given 'Unique'.
+uniqueNameSpace :: Unique -> Char
+uniqueNameSpace (Unique u) =
+  chr ((u `shiftR` 24) .&. 0x7f)
 
 mk_unique :: Char -> Int -> Unique
-mk_unique c n | ord c < 256 = Unique u
+mk_unique c n | ord c < 128 = Unique u
  where u = (ord c `shiftL` 24) .|. (n .&. 0x00ffffff)
 
 bogusUnique :: Unique
@@ -45,9 +86,9 @@ intFromUnique :: Unique -> Int
 intFromUnique (Unique n) = n
 
 baseXDigits :: UArray Int Char
-baseXDigits = listArray (0, n) digits
+baseXDigits = listArray (0, n-1) digits
   where
-    digits = ['0'..'9']++['a'..'z']++['A'..'Z']
+    digits = ['A'..'Z']++['a'..'z']++['0'..'9']++['+','$']
     n = length digits
     -- TODO: filter out 0/O 1/l ?
 
@@ -58,5 +99,5 @@ baseX = snd (bounds baseXDigits) + 1
 toBaseXString :: Int -> String
 toBaseXString n | n < baseX = [baseXDigits ! n]
 toBaseXString n =
-  let (n', offs) = n `divMod` baseX in
+  let (n', offs) = n `quotRem` baseX in
   baseXDigits ! offs : toBaseXString n'
