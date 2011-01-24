1385df959e36f8734213028e7dd69198344ae46c
Thomas Schilling <nominolo@googlemail.com>
2011-01-24 12:17:23 +0000

    Benchmark tweaks.

--- tests/Bench/Append.hs
+++ tests/Bench/Append.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude, BangPatterns, MagicHash #-}
+{-# OPTIONS_GHC -O0 #-}
 module Bench.Append where
 
 import GHC.Types
@@ -8,15 +9,21 @@ import GHC.Base
 
 root xs ys zs = (xs ++ ys) ++ zs
 
+{- # NOINLINE replicate #-}
 replicate :: Int -> Int -> [Int]
 replicate n x =
   if n == 0 then [] else x : replicate (n - 1) x
 
+{- # NOINLINE length #-}
 length :: [a] -> Int
-length [] = 0
-length (x:xs) = 1 + length xs
+length l = len l 0#
+  where
+    len []     a# = I# a#
+    len (_:xs) a# = len xs (a# +# 1#)
 
-test =
-  let n = 25 in
+{- # NOINLINE test1 #-}
+test1 n =
   length (root (replicate n 1) (replicate n 2) (replicate n 3))
     == (n + n + n)
+
+test = test1 20 -- max: 1820
