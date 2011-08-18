2b7837830744490ddf7e9a2d11841f12cda877e0
David M Peixotto <dmp@rice.edu>
2011-08-18 16:05:49 -0500

    Updated debugging settings

--- rts/codegen/InterpAsm.c
+++ rts/codegen/InterpAsm.c
@@ -17,8 +17,8 @@ extern void asmEnter(Fragment *F, Thread *T, Word *spillArea, Word *base, MCode*
 void asmEngine(Capability *cap, Fragment *F) {
   JitState *J = &cap->J;
 
-  IF_DBG_LVL(2,dumpAsm(F->mcode, F->szmcode, NULL /* use a new FILE */));
-  IF_DBG_LVL(2,dumpExitStubs(J));
+  IF_DBG_LVL(1,dumpAsm(F->mcode, F->szmcode, NULL /* use a new FILE */));
+  IF_DBG_LVL(1,dumpExitStubs(J));
   enterTrace(J, F);
 }
 
