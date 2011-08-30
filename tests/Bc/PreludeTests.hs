a8adcd541a23349e285f70a91432d240232c5d58
Thomas Schilling <nominolo@googlemail.com>
2011-08-31 00:17:35 +0100

    Adjust expected answer for Bc/PreludeTests.hs.

--- tests/Bc/PreludeTests.hs
+++ tests/Bc/PreludeTests.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude, BangPatterns, MagicHash #-}
 -- RUN: %bc_vm_chk
--- CHECK: @Result@ IND -> GHC.Types.I#`con_info 0x000000000000000a
+-- CHECK: @Result@ IND -> GHC.Types.I#`con_info 10
 module Bc.PreludeTests where
 
 import GHC.Base
