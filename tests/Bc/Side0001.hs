aa40b2508a0c723b1a8d0572126aa830dc4c9b1a
Thomas Schilling <nominolo@googlemail.com>
2012-05-28 02:06:54 +0100

    Add side trace test case.

---
+++ tests/Bc/Side0001.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NoImplicitPrelude, BangPatterns, MagicHash #-}
+-- RUN: %bc_vm_chk
+-- CHECK: @Result@ IND -> GHC.Bool.True`con_info
+module Bc.Side0001 where
+
+import GHC.Prim
+import GHC.Types
+import GHC.Base
+
+-- import Debug.Trace
+-- import Prelude
+
+-- Test for side traces
+
+-- Emulates Bresenham line drawing without actually drawing any pixels.
+loop :: Int# -> Int# -> Int# -> Int# -> Int# -> Int
+--loop x  y d _ _ | trace (show (I# x, I# y, I# d)) False = undefined
+loop 0# y d up down = I# y
+loop x  y d up down =
+  if d <=# 0# then loop (x -# 1#) y        (d +# up)   up down
+              else loop (x -# 1#) (y +# 1#) (d -# down) up down
+
+test = loop 500# 0# 1# 3# 4# == 214
