cff68c358a496d38f66f2cef41086e33ae941b15
Thomas Schilling <nominolo@googlemail.com>
2012-06-23 02:14:34 +0100

    Check result more thoroughly in Bc.Gc01.

--- tests/Bc/Gc01.hs
+++ tests/Bc/Gc01.hs
@@ -15,4 +15,10 @@ f 0# acc = acc
 f n acc = let !acc' = I# n : acc in
           f (n -# 1#) acc'
 
-test = length (f 4000# []) == 4000
+check :: Int# -> Int# -> [Int] -> Bool
+check len n [] = n ==# (len +# 1#)
+check len n (I# m:xs) =
+  if n ==# m then check len (n +# 1#) xs else False
+
+test =
+  let !n = 4000# in check n 1# (f n [])
