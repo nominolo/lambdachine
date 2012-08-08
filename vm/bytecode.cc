b39910243f985cf78f7a86e86fde505a6c26876f
Thomas Schilling <nominolo@googlemail.com>
2012-08-09 00:10:10 +0100

    Fix style in bytecode.cc.

--- vm/bytecode.cc
+++ vm/bytecode.cc
@@ -49,7 +49,7 @@ static ostream &printInlineBitmaps(ostream &out, const BcIns *ins) {
   }
 
   out << '\t';
-  const u2 *ptr_bitmap = (const u2*)((u1*)ins + offset);
+  const u2 *ptr_bitmap = (const u2 *)((u1 *)ins + offset);
   const u2 *lives_bitmap = NULL;
   // Find successor bitmap
   for (lives_bitmap = ptr_bitmap; (*lives_bitmap & 0x8000) != 0;
@@ -101,7 +101,7 @@ const BcIns *BcIns::debugPrint(ostream &out, const BcIns *ins,
     break;
   case IFM_RRR:
     out << i.name() << "\tr" << (int)i.a() << ", r" << (int)i.b()
-        << ", r" << (int)i.c()<< endl;
+        << ", r" << (int)i.c() << endl;
     break;
   case IFM_RN:
     out << i.name() << "\tr" << (int)i.a() << ", " << (int)i.d();
@@ -116,7 +116,7 @@ const BcIns *BcIns::debugPrint(ostream &out, const BcIns *ins,
     break;
   case IFM_RRN:
     out << i.name() << "\tr" << (int)i.a() << ", r" << (int)i.b()
-        << ", " << (int)i.c()<< endl;
+        << ", " << (int)i.c() << endl;
     break;
   case IFM_J:
     out << i.name() << " ->";
@@ -135,22 +135,21 @@ const BcIns *BcIns::debugPrint(ostream &out, const BcIns *ins,
       ++ins;
       printInlineBitmaps(out, ins - 1);
       break;
-    case kCASE:
-      {
-        u2 *tgt = (u2*)ins;
-        u4 ncases = i.d();
-        ins += (ncases + 1) / 2;
-        out << "CASE\tr" << (int)i.a()
-            << " [tags 1.." << (int)i.d() << "]" << endl;
-        if (!oneline) {
-          for (u4 j = 0; j < ncases; j++, tgt++) {
-            out << "           " << j + 1
-                << ": ->";
-            printAddr(out, baseaddr, ins + (int)(*tgt)) << endl;
-          }
+    case kCASE: {
+      u2 *tgt = (u2 *)ins;
+      u4 ncases = i.d();
+      ins += (ncases + 1) / 2;
+      out << "CASE\tr" << (int)i.a()
+          << " [tags 1.." << (int)i.d() << "]" << endl;
+      if (!oneline) {
+        for (u4 j = 0; j < ncases; j++, tgt++) {
+          out << "           " << j + 1
+              << ": ->";
+          printAddr(out, baseaddr, ins + (int)(*tgt)) << endl;
         }
       }
-      break;
+    }
+    break;
     case kCASE_S:
       out << "CASE_S\tr" << (int)i.a() << " ...TODO..." << endl;
       ins += i.d();
@@ -161,63 +160,59 @@ const BcIns *BcIns::debugPrint(ostream &out, const BcIns *ins,
       ++ins;  // skip bitmap
       printInlineBitmaps(out, ins - 1);
       break;
-    case kALLOC:
-      {
-        u1 *arg = (u1*)ins;
-        ins += 1 + BC_ROUND(i.c());
-        out << "ALLOC\tr" << (int)i.a() << ", r" << (int)i.b();
-        for (u4 j = 0; j < i.c(); j++, arg++) {
-          out << ", r" << (int)*arg;
-        }
-        printInlineBitmaps(out, ins - 1);
+    case kALLOC: {
+      u1 *arg = (u1 *)ins;
+      ins += 1 + BC_ROUND(i.c());
+      out << "ALLOC\tr" << (int)i.a() << ", r" << (int)i.b();
+      for (u4 j = 0; j < i.c(); j++, arg++) {
+        out << ", r" << (int)*arg;
       }
-      break;
-    case kALLOCAP:
-      {
-        u1 *arg = (u1*)ins;
-        ins += 1 + BC_ROUND(i.c() + 1);
-        out << "ALLOCAP\tr" << (int)i.a();
-        u1 ptrmask = i.b();
-        out << ", r" << (int)*arg++;
-        for (u4 j = 1; j < (u4)i.c() + 1; j++, arg++) {
-          out << ", r" << (int)*arg;
-          if (ptrmask & 1) out << '*';
-          ptrmask >>= 1;
-        }
-        printInlineBitmaps(out, ins - 1);
+      printInlineBitmaps(out, ins - 1);
+    }
+    break;
+    case kALLOCAP: {
+      u1 *arg = (u1 *)ins;
+      ins += 1 + BC_ROUND(i.c() + 1);
+      out << "ALLOCAP\tr" << (int)i.a();
+      u1 ptrmask = i.b();
+      out << ", r" << (int)*arg++;
+      for (u4 j = 1; j < (u4)i.c() + 1; j++, arg++) {
+        out << ", r" << (int)*arg;
+        if (ptrmask & 1) out << '*';
+        ptrmask >>= 1;
       }
-      break;
-    case kCALL:
-      {
-        u1 *arg = (u1*)ins;
-        ins += BC_ROUND(i.c()) + 1;
-        out << "CALL\tr" << (int)i.a();
-        u1 ptrmask = i.b();
-        char comma = '(';
-        for (u4 j = 0; j < i.c(); j++, arg++) {
-          out << comma << "r" << (int)*arg;
-          if (ptrmask & 1) out << '*';
-          comma = ',';
-          ptrmask >>= 1;
-        }
-        out << ")";
-        printInlineBitmaps(out, ins - 1);
+      printInlineBitmaps(out, ins - 1);
+    }
+    break;
+    case kCALL: {
+      u1 *arg = (u1 *)ins;
+      ins += BC_ROUND(i.c()) + 1;
+      out << "CALL\tr" << (int)i.a();
+      u1 ptrmask = i.b();
+      char comma = '(';
+      for (u4 j = 0; j < i.c(); j++, arg++) {
+        out << comma << "r" << (int)*arg;
+        if (ptrmask & 1) out << '*';
+        comma = ',';
+        ptrmask >>= 1;
       }
-      break;
-    case kCALLT:
-      {
-        u1 bitmask = i.b();
-        out << "CALLT r" << (int)i.a();
-        char comma = '(';
-        for (u4 j = 0; j < i.c(); j++) {
-          out << comma << 'r' << j;
-          if (bitmask & 1) out << '*';
-          comma = ',';
-          bitmask >>= 1;
-        }
-        out << ')' << endl;
+      out << ")";
+      printInlineBitmaps(out, ins - 1);
+    }
+    break;
+    case kCALLT: {
+      u1 bitmask = i.b();
+      out << "CALLT r" << (int)i.a();
+      char comma = '(';
+      for (u4 j = 0; j < i.c(); j++) {
+        out << comma << 'r' << j;
+        if (bitmask & 1) out << '*';
+        comma = ',';
+        bitmask >>= 1;
       }
-      break;
+      out << ')' << endl;
+    }
+    break;
     case kFUNCPAP:
     case kSTOP:
       out << i.name() << endl;
