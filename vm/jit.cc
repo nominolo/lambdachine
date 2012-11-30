b0f58c046a577c25883c7ea90d108c4e823d2a4f
Thomas Schilling <nominolo@googlemail.com>
2012-11-30 23:45:39 +0000

    Record indirections.  Might be a bad idea.

--- vm/jit.cc
+++ vm/jit.cc
@@ -240,6 +240,18 @@ static bool evalCond(BcIns::Opcode opc, Word left, Word right) {
   }
 }
 
+static inline Closure *
+followIndirection(IRBuffer &buf_, int slot, Closure *tnode)
+{
+  TRef noderef = buf_.slot(slot);
+  TRef inforef = buf_.literal(IRT_INFO, (Word)tnode->info());
+  buf_.emit(IR::kEQINFO, IRT_VOID | IRT_GUARD, noderef, inforef);
+  TRef fwdref = buf_.emit(IR::kFREF, IRT_PTR, noderef, 1);
+  TRef newnoderef = buf_.emit(IR::kFLOAD, IRT_CLOS, fwdref, 0);
+  buf_.setSlot(slot, newnoderef);
+  return (Closure *)tnode->payload(0);
+}
+
 bool Jit::recordGenericApply(uint32_t call_info, Word *base,
                              TRef fnode_ref, Closure *fnode,
                              const Code *code) {
@@ -499,9 +511,8 @@ bool Jit::recordIns(BcIns *ins, Word *base, const Code *code) {
 
   case BcIns::kEVAL: {
     Closure *tnode = (Closure *)base[ins->a()];
-    if (tnode->isIndirection()) {
-      logNYI(NYI_RECORD_EVAL_IND);
-      goto abort_recording;
+    while (tnode->isIndirection()) {
+      tnode = followIndirection(buf_, ins->a(), tnode);
     }
     TRef noderef = buf_.slot(ins->a());
     TRef inforef = buf_.literal(IRT_INFO, (Word)tnode->info());
