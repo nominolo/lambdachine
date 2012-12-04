3c12e79fda28686467b9ab94975043ea9a568da8
Thomas Schilling <nominolo@googlemail.com>
2012-12-04 21:57:34 +0000

    Decode closures when using `lcvm --trace`.

--- vm/main.cc
+++ vm/main.cc
@@ -69,6 +69,7 @@ int main(int argc, char *argv[]) {
 
   if (opts->traceInterpreter()) {
     cap.enableBytecodeTracing();
+    cap.enableDecodeClosures();
   }
 
   Time start_time = getProcessElapsedTime();
