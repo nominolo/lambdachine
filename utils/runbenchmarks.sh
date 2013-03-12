30e6d301b286acd0db354847bab6704b797d6c08
Thomas Schilling <nominolo@googlemail.com>
2013-03-12 16:42:52 +0100

    Update utils/runbenchmarks.sh

--- utils/runbenchmarks.sh
+++ utils/runbenchmarks.sh
@@ -3,18 +3,30 @@
 BENCHMARKS="Bench.SumFromTo1
 Bench.SumFromTo2
 Bench.SumSquare1
-Bench.Nofib.Spectral.Constraints
+Bench.SumStream
+Bench.Tak
+Bench.WheelSieve2
 Bench.Nofib.Spectral.Boyer
-Bench.Nofib.Spectral.Lambda
+Bench.Nofib.Spectral.Constraints
 Bench.Nofib.Spectral.Circsim
-Bench.Fibon.Agum.Main"
+Bench.Nofib.Spectral.Lambda"
+
 for bench in ${BENCHMARKS}; do
-    echo "=== ${bench}"
-    ./lcvm -e bench ${bench} --stack=10m
+    bench_file=tests/`echo "${bench}" | sed 's;\.;/;g'`.hs
+    ./lcc -o2 ${bench_file}
 done
 
-BENCHMARKS2="Bench.Fibon.Agum.Main"
-for bench in ${BENCHMARKS2}; do
-    echo "=== ${bench}"
-    ./lcvm -e test ${bench} --stack=10m
+# Bench.Fibon.Agum.Main"
+# BENCHMARKS="Bench.SumFromTo1"
+for bench in ${BENCHMARKS}; do
+    for run in 1 2 3 4 5; do
+        echo "=== ${bench} === run ${run} ==="
+        ./lcvm -e bench ${bench} --stack=10m
+    done
 done
+
+# BENCHMARKS2="Bench.Fibon.Agum.Main"
+# for bench in ${BENCHMARKS2}; do
+#     echo "=== ${bench}"
+#     ./lcvm -e test ${bench} --stack=10m
+# done
